62b520aaf9cbaf8012670ddec03e19b81e9fe2e3
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2019-06-30 16:46:37 +0200

    Drop bespoke unsafe function that was used for RLE decoding, it's no longer needed

--- src/util.rs
+++ src/util.rs
@@ -2,17 +2,6 @@
 use std::io::{self, Read};
 use std::ptr;
 
-#[inline]
-pub unsafe fn ptr_copy(src: *const u8, dst: *mut u8, count: usize, is_overlapping: bool) {
-    if !is_overlapping {
-        ptr::copy_nonoverlapping(src, dst, count);
-    } else {
-        for i in 0..count {
-            ptr::copy_nonoverlapping(src.add(i), dst.add(i), 1);
-        }
-    }
-}
-
 #[cfg(test)]
 pub struct WouldBlockReader<R> {
     inner: R,
