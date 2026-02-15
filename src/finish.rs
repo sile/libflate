3e7a62281ae9cf85cb9fda5efa7ce88cd774d49b
Takeru Ohta <phjgt308@gmail.com>
2026-02-15 20:06:21 +0900

    chore: Upgrade Rust edition to 2024 and update actions/checkout to v6

--- src/finish.rs
+++ src/finish.rs
@@ -125,11 +125,10 @@ impl<T: Complete> AutoFinish<T> {
 }
 impl<T: Complete> Drop for AutoFinish<T> {
     fn drop(&mut self) {
-        if let Some(inner) = self.inner.take() {
-            if let Err(e) = inner.complete() {
+        if let Some(inner) = self.inner.take()
+            && let Err(e) = inner.complete() {
                 panic!("{}", e);
             }
-        }
     }
 }
 impl<T: Complete> Deref for AutoFinish<T> {
